449e5370bcbda021b29158e317cdf0ea7c3ae976
Eirik Kvistad <eirik.kvistad@gmail.com>
2020-09-08 12:40:48 +0200

    added firebase

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDoFQKLFC0pFyjWpFCDf26_816I3K1f5vI",
+    authDomain: "webproject3-c4d5d.firebaseapp.com",
+    databaseURL: "https://webproject3-c4d5d.firebaseio.com",
+    projectId: "webproject3-c4d5d",
+    storageBucket: "webproject3-c4d5d.appspot.com",
+    messagingSenderId: "764434104895",
+    appId: "1:764434104895:web:b7da43e1eeb3ea90b8ee8a",
+    measurementId: "G-4XZQQYP21B"
+  };
+
+  export default firebaseConfig
