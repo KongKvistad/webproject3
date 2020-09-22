06f662b80a7bbea3b33c30197b68bb85b2ff7836
Marcusls <marcus.swensen@gmail.com>
2020-09-22 12:29:25 +0200

    Testpush

---
+++ src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<!--
+*
+* @desc Navigation bar at the top of every site
+*
+-->
+
+<template>
+    <nav>
+        <div><img id="logo"></div>
+        <ul>
+            <li>Discover</li>
+            <li>Marketplace</li>
+            <li>Social</li>
+        </ul>
+        <ull>
+            <li>Profile</li>
+        </ull>
+
+    </nav>
+</template>
+
+<script>
+import firebase from 'firebase';
+export default {
+    name: 'NavBar', // name of component to be used elsewhere
+    data() {
+
+    },
+
+    methods: {
+
+    },
+
+
+    
+}
+</script>
+
+<style scoped>
+
+</style>
