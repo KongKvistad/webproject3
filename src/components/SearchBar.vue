2b1d21edcdf99a3b2af4e6bc6cc278f6a162287e
Lars Erik Lunde <larserik@lel-mbp.home>
2020-09-29 23:24:00 +0200

    Start home page

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,10 +1,27 @@
 <template>
+  <div class="box pt-6">
+    <div class="box-wrapper">
+      <div class=" bg-white rounded flex items-center w-full p-3 shadow-sm border border-gray-200">
+        <button @click="PLACEHOLDER" class="outline-none focus:outline-none"><svg class=" w-5 text-gray-600 h-5 cursor-pointer" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
+        <input type="search" name="" id="" @keydown.enter="PLACEHOLDER" placeholder="Search.." x-model="q" class="w-full pl-4 text-sm outline-none focus:outline-none bg-transparent">
+        <div class="select">
+          <select name="" id="" x-model="image_type" class="text-sm outline-none focus:outline-none bg-transparent">
+            <option value="all" selected>All</option>
+            <option value="work">Work</option>
+            <option value="exchange">Exchange</option>
+            <option value="study programme">Study Programme</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  </div>
 
+<!--
     <div class="pt-2 relative mx-auto text-gray-600">
-        <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+        <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-6 rounded-lg text-sm focus:outline-none"
           type="search" name="search" placeholder="Search">
         <button type="submit" class="absolute right-0 top-0 mt-5 mr-4">
-          <svg class="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+          <svg class="text-gray-600 h-4 w-8 fill-current" xmlns="http://www.w3.org/2000/svg"
             xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
             viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve"
             width="512px" height="512px">
@@ -13,7 +30,7 @@
           </svg>
         </button>
     </div>
-      
+-->      
 
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,74 @@
 <template>
-  <main>
-       <SearchBar/>
-       <label class="select-none container block relative cursor-pointer text-xl pl-8">Option
-        <input class="absolute opacity-0 left-0 top-0 cursor-pointer" type="checkbox" checked="checked">
-        <span class="h-6 w-6 checkmark absolute top-0 left-0 bg-gray-400"></span>
-        </label>
-  </main>
-</template>
+    <main>
+        <div class="banner-area"> <!-- :style="image">  Virker til å vise bilde på vue måten men da virker ikke css'en-->
+          <div class="content-area">
+            <div class="content">
+              <SearchBar/>
+              <!-- Checkbox
+              <label class="select-none container block relative cursor-pointer text-xl pl-8">Option
+                  <input class="absolute opacity-0 left-0 top-0 cursor-pointer" type="checkbox" checked="checked">
+                  <span class="h-6 w-6 checkmark absolute top-0 left-0 bg-gray-400"></span>
+              </label>
+              -->
+              <div class="headline">Opportunities abroad.</div>
+              <div class="under-headline">Made easy.</div>
+            </div>
+          </div>
+        </div>
+  
+  
+        <div class="content-suggestion">
+          <h1>Maybe you're interested in...</h1>
+          <p>E-courses</p>
+          <!--
+          <div class="content-suggestion-cards">
+            <div v-for="item in placeholder" :key="item.id" >
+              <p>{{ item.id }} </p>
+              <p>{{ item.content }}</p>
+              <p> {{ item.price }}</p>
+            </div>
+          </div>
+        -->
+          <div class="smallCard">
+            <SmallCard v-for="item in placeholder" :key="item.id" :item="item"/>
+          </div>
+        </div>
+        
+        
+
+    </main>
+  
+  </template>
 
 <script>
 //import db from '@/components/firebaseInit.js'
 //import {getCollections} from '../helpers/collections.js'
 import {db} from '../components/firebaseInit.js'
 import SearchBar from '../components/SearchBar.vue'
+//import EngagementTypeBox from '../components/EngagementTypeBox.vue'
+import SmallCard from '../components/SmallCard.vue'
 
 export default {
   name: 'Home',
   components: {
     SearchBar,
+    SmallCard,
+    //EngagementTypeBox,
   },
 
   data(){
     return {
       results:[],
       cityOrCountry: false,
-      resCount: 0
+      resCount: 0,
+      //image: {
+      //  backgroundImage: `url(${require('@/assets/students-park.jpg')})`
+      //},
+      placeholder: [ 
+        { id: 1, img: '@/assests/logo.png', content: 'How to write a good application for Uni and stuff..', price: '34' },
+        { id: 2, img: '@/assests/logo.png', content: 'How to write a good application for Uni and stuff..', price: '34' },
+        { id: 3, img: '@/assests/logo.png', content: 'How to write a good application for Uni and stuff..', price: '34' }
+      ]
     }
   },
   
@@ -71,42 +115,90 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
- main{
-     background-color:blue;
-    display: flex;
-    height: calc(100vh - 1em);
-    width: 100vw;
- }
+  <style scoped>
 
+    main {
+      width: auto;
+      margin: auto;
+      /*
+       height: calc(100vh - 1em);
+       width: 100vw;
+       */
+    }
+   
+   /* Search bar and main content text */
+   .banner-area {
+     width: 100%;
+     height: 100vh;
+     background-image:linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(../assets/students-park.jpg);
+     background-size: cover;
+     background-position: center;
+     opacity: 90;
+   }
+   
+   .content-area {
+      display: flex;
+     justify-content: center;
+     flex-direction: column;
+     align-items: center;
+   }
+     
+   .content {
+     text-align: center;
+     margin-top: 20%;
+     width: 60%;
+   }
+   
+   
+   .headline {
+     padding-right: 1rem;
+     padding-left: 1rem;
+     margin-top: 2rem;
+     font-size: 4vw;
+     background-color: #5E80F8;
+     color: white;
+     text-align: left;
+   }
+   
+   .under-headline {
+     padding: 0.5rem;
+     margin-top: 1rem;
+     font-size: 2vw;
+     float: left;
+     background-color: #5E80F8;
+     color: white;
+     text-align: left;
+   }
+   
+   
+   /* Content suggestions */
+   
+   .content-suggestion {
+      background-color: white;
+    }
 
-.container:hover input ~ .checkmark {
-  background-color: #a0aec0;
-}
+    .content-suggestion > h1 {
+      font-size: 36px;
+      padding: 2%;
+      text-align: center;
+    }
 
-.container input:checked ~ .checkmark {
-  background-color: #0078d4;
-}
+    .content-suggestion > p {
+      font-size: 24px;
+      padding: 3%;
+      margin-left: 3%;
+    }
+   
+  
 
-.checkmark:after {
-  content: "";
-  position: absolute;
-  display: none;
-}
-
-.container input:checked ~ .checkmark:after {
-  display: block;
-}
-
-.container .checkmark:after {
-  left: 9px;
-  top: 6px;
-  width: 5px;
-  height: 10px;
-  border: solid white;
-  border-width: 0 3px 3px 0;
-  -webkit-transform: rotate(45deg);
-  -ms-transform: rotate(45deg);
-  transform: rotate(45deg);
-}
-</style>
+   .content-suggestion-cards {
+      
+    }
+   
+   .smallCard {
+     display: flex;
+     flex-direction: columns;
+   }
+   
+   </style>
+   
